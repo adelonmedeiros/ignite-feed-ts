2219cb1f46847cb298b322d381b8596b421b2295
Adelon Medeiros <adelon.medeiros@compasso.com.br>
2023-02-27 18:19:16 -0300

    fix: Correções TypeScript e melhorias na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
@@ -10,7 +10,7 @@ import "./global.css";
 // content: String
 // publishedAt: Date
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -58,14 +58,7 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
